4425a1012cb9fff721593a1050ee3ba9b6c33fa9
surya <surya.vme005@gmail.com>
2023-08-29 19:04:50 +0530

    file created

--- alphabet_pattern.cpp
+++ alphabet_pattern.cpp
@@ -1,6 +1,38 @@
 #include<iostream>>
 using namespace std;
+
+pattern(int n)
+{
+    if(n<3)
+    {
+        cout<<" Please enter the valid Number for pramid... ";
+    }
+    else
+    {
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+         int m=1;
+        for(int k=1;k<=i;k++)
+        {
+            cout<<m++;
+        }
+          m=m-2;
+        for(int l=1;l<i;l++)
+        {
+            cout<<m--;
+        }
+        cout<<"\n";
+    }
+  }
+}
 int main()
 {
-
+   int n;
+   cout<<"Enter the Number :";
+   cin>>n;
+   pattern(n);
 }
